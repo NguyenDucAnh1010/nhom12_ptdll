88c630913314b2496b997ea690b445f860df932f
thanhst <thanhxk2003@gmail.com>
2024-10-24 18:04:14 +0700

    update

--- abcd/import_cmd.py
+++ abcd/import_cmd.py
@@ -4,7 +4,8 @@ import subprocess
 # Hàm thực thi lệnh spark-submit bên trong container Docker và chỉ lấy kết quả
 def run_spark_job():
     # Chạy lệnh spark-submit với truy vấn SQL được chọn
-    command = 'docker exec -it spark-master bash -c "spark-submit --master spark://spark-master:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 /opt/shared/import.py"'
+    command = "docker exec -it spark-master bash -c \"spark-submit --master spark://spark-master:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 /opt/shared/import.py\""
+    
     try:
         subprocess.run(command, shell=True, capture_output=True, text=True)
     except Exception as e:
